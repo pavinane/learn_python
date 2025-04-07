054e9d16d4800d12e5add4bf42ec3ad9321b77ba
pavithranath <pavithranath@cloudrevelinnovation.com>
2025-04-07 17:36:11 +0530

    input casting

---
+++ hellow/input_casting.py
@@ -0,0 +1,43 @@
+# Example
+
+# a = int(input())
+# b =  int(input())
+
+# c= a+ b
+# print(f"result",c)
+
+
+# Example 2
+
+# name = input()
+# age = input()
+# address = input()
+
+# print(f"My name is :  {name}")
+# print (f"My age is : {age}")
+# print ("My address is :",address)
+
+
+# Example 3
+
+# a = int(input())
+# b =  int(input())
+# c =  int(input())
+
+# mul = a*b*c
+# print ("multiplication",mul)
+# add = a+b+c
+# print ("addition",add)
+# div = mul/add
+# print ("division",div)
+
+
+# Example 4
+
+name = input()
+score =  int(input())
+department =  input()
+
+print(f"My name is :  {name}")
+print (f"My Score is : {score/10}")
+print ("My Department is :",department)

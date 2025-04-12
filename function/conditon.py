1e3692e249b5177d225ea4fd9a31b5cda3e431b8
unknown <pavimegan77@gmail.com>
2025-04-12 13:02:42 +0530

    operators and conditions

---
+++ function/conditon.py
@@ -0,0 +1,35 @@
+# def painter(message):
+#     print(message)
+
+# painter("paint on my house")
+
+
+# def evn(value):
+    
+#     if(value % 2 ==0):
+#         print(f"{value} is even")
+#     else:
+#         print(f"{value} is odd")
+
+# a= 15
+# evn(a)
+
+# def passorfail(score):
+#     if(score >35):
+#         print("pass")
+#     else:
+#         print("fail")
+
+# mark = int(input("Enter score : "))
+# passorfail(mark)
+
+def printrange(a,b):
+    for i in range(a,b):
+        print(i)
+
+
+a = int(input("Enter a value a"))
+b = int(input("Enter a value b"))
+
+
+printrange(a,b)
---
+++ function/operators.py
@@ -0,0 +1,23 @@
+# def add():
+#     a = int(input("Enter a value"))
+#     b = int(input("Enter b value"))
+#     print(a+b)
+
+# add()
+
+# def sub():
+#     a = int(input("Enter a value"))
+#     b = int(input("Enter b value"))
+#     print(a-b)
+
+# sub()
+
+# def mul():
+#     a = int(input("Enter a value"))
+#     b = int(input("Enter b value"))
+#     print(a*b)
+
+# mul()
+
+
+
